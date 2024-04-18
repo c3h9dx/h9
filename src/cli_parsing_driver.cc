e436312c9102b9a90af4da2d24b7fd378d4aa29d
crowx <kamil.palkowski@gmail.com>
2024-04-18 10:21:03 +0200

    Linux fix

--- src/cli_parsing_driver.cc
+++ src/cli_parsing_driver.cc
@@ -128,12 +128,12 @@ char* CLIParsingDriver::unescape(const char* str) {
     unescaped[j] = '\0';
 
     char* resized_unescaped;
-    if ((resized_unescaped = static_cast<char*>(realloc(unescaped, j))) == nullptr) {
+    if ((resized_unescaped = static_cast<char*>(realloc(unescaped, j))) == nullptr && j != 0) {
         free(unescaped);
         resized_unescaped = nullptr;
         SPDLOG_ERROR("realloc error");
     }
-
+    ;
     return resized_unescaped;
 }
 
